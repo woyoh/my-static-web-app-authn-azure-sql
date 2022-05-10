0baab15b4b5bc2408efeaff3850c9b7631240315
WangYuHeng <d-wangyh@cncsys.com.cn>
2022-05-10 13:30:25 +0800

    update0509

--- api/products-get/index.js
+++ api/products-get/index.js
@@ -22,24 +22,6 @@ const executeSQL = (context, verb, entity, payload) => {
     const paramPayload = (payload != null) ? JSON.stringify(payload) : '';
     context.log("payload: " + payload);
 
-    // // Create Connection object
-    // const connection = new Connection({
-    //     server: process.env["db_server"],
-    //     authentication: {
-    //         type: 'default',
-    //         options: {
-    //             userName: process.env["db_user"],
-    //             password: process.env["db_password"],
-    //         }
-    //     },
-    //     options: {
-    //         database: process.env["db_database"],
-    //         encrypt: true,
-    //         connectTimeout: 15000,
-    //         validateBulkLoadParameters: true
-    //     }
-    // });
-
     let config = {
         server: process.env["db_server"],
         authentication: {
@@ -57,58 +39,41 @@ const executeSQL = (context, verb, entity, payload) => {
         }
     };
     
-    // let config = {
-    //     server: "helloserver01.database.windows.net",
-    //     authentication: {
-    //         type: 'default',
-    //         options: {
-    //             userName: "NodeFuncApp",
-    //             password: "aN0ThErREALLY#$%TRONGpa44w0rd!",
-    //         }
-    //     },
-    //     options: {
-    //         database: "TutorialDB",
-    //         encrypt: true,
-    //         connectTimeout: 15000,
-    //         validateBulkLoadParameters: true
-    //     }
-    // };
-    
     // Create Connection object
     const connection = new Connection(config);
 
-    // Call Procedure
-    const request = new Request(`web.${verb}_${entity}`, (err) => {
-        if (err) {
-            context.log.error(err);            
-            context.res.status = 500;
-            context.res.body = "Error executing T-SQL command";
-        } else {
-            context.res = {
-                body: result
-            }
-        }
-        context.done();
-        console.log("Context BODY: " + context.res.body);
-    });    
-    if (payload) {
-        request.addParameter('Json', TYPES.NVarChar, paramPayload, Infinity);
-    }
-
-    // // SQL
-    // var request = new Request("select * from dbo.Product", function(err) {
+    // // Call Procedure
+    // const request = new Request(`web.${verb}_${entity}`, (err) => {
     //     if (err) {
-    //         console.log(err);
+    //         context.log.error(err);            
     //         context.res.status = 500;
-    //         context.res.body = "Error executing SQL command";
-    //    } else {
+    //         context.res.body = "Error executing T-SQL command";
+    //     } else {
     //         context.res = {
     //             body: result
     //         }
     //     }
     //     context.done();
-    //     console.log(result);
-    // });
+    //     console.log("Context BODY: " + context.res.body);
+    // });    
+    // if (payload) {
+    //     request.addParameter('Json', TYPES.NVarChar, paramPayload, Infinity);
+    // }
+
+    // Execute SQL
+    var request = new Request("select * from dbo.Product", function(err) {
+        if (err) {
+            console.log(err);
+            context.res.status = 500;
+            context.res.body = "Error executing SQL command";
+       } else {
+            context.res = {
+                body: result
+            }
+        }
+        context.done();
+        console.log("Result:" + result);
+    });
     
     // Handle 'connect' event
     connection.on('connect', err => {
@@ -119,11 +84,8 @@ const executeSQL = (context, verb, entity, payload) => {
             context.done();
         }
         else {
-            // Connection succeeded so execute T-SQL stored procedure
-            // if you want to executer ad-hoc T-SQL code, use connection.execSql instead
-            
-            connection.callProcedure(request);
-            // connection.execSql(request);
+            // connection.callProcedure(request);
+            connection.execSql(request);
         }
     });
 
